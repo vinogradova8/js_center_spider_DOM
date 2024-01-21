5df3221b4517c6fd0dd03e1e1b0d0798fae90f43
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-01-21 11:53:57 +0200

    make build

---
+++ index.7056045a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientWidth,l=t.clientHeight,i=e.clientWidth,n=e.clientHeight;e.style.left="".concat(c/2-i/2,"px"),e.style.top="".concat(l/2-n/2,"px");
+//# sourceMappingURL=index.7056045a.js.map
